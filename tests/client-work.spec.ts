a0ed8b1e2508c61d1502fa0d5ff70c7df6c80bff
Nagaraj-test-git <anbuvel1696@gmail.com>
2026-03-16 14:16:27 +0530

    Create tests/client-work.spec.ts

---
+++ tests/client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM - Client Work navigation', () => {
+  test('navigates to Client Work and verifies text', async ({ page }) => {
+    /* TEST_BODY_PLACEHOLDER */
+  });
+});
